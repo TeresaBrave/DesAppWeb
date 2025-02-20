735144cc2f4f11c5ce8aada8393fe01d21f73c5a
Teresa Domingos <teresadmngs93@gmail.com>
2025-02-20 10:18:01 +0100

    aa

--- php/Examen_Filmin/contacto.php
+++ php/Examen_Filmin/contacto.php
@@ -1,17 +1,22 @@
+<? require 'bloques/config.php'; ?>
+<? include 'bloques/header.php'; ?>
+
+<h1>Contacto</h1>
+<p>¡Nos encantaría saber de ti! Completa el formulario y nos pondremos en contacto contigo lo antes posible.</p>
+
 <section id="contacto">
-  <h2>Contacto</h2>
-  <p>Completa el formulario y nos pondremos en contacto contigo lo antes posible.</p>
-  
-  <form>
-    <label for="nombre">Nombre:</label>
-    <input type="text" id="nombre" name="nombre" placeholder="Tu nombre" required>
+    <form method="post" action="enviar.php">
+        <label for="nombre">Nombre:</label>
+        <input type="text" id="nombre" name="nombre" placeholder="Tu nombre" required>
 
-    <label for="email">Correo Electrónico:</label>
-    <input type="email" id="email" name="email" placeholder="Tu correo" required>
+        <label for="email">Correo Electrónico:</label>
+        <input type="email" id="email" name="email" placeholder="Tu correo" required>
 
-    <label for="mensaje">Mensaje:</label>
-    <textarea id="mensaje" name="mensaje" placeholder="Escribe tu mensaje" required></textarea>
+        <label for="mensaje">Mensaje:</label>
+        <textarea id="mensaje" name="mensaje" placeholder="Escribe tu mensaje" required></textarea>
 
-    <button type="submit">Enviar</button>
-  </form>
+        <button type="submit">Enviar</button>
+    </form>
 </section>
+
+<?php include_once 'bloques/footer.php'; ?>
